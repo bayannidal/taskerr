5dc8622df215973571ce0fa52813cfdf4a9b7f97
Nidal Bayan <bayan_nidal@yahoo.com>
2022-04-15 01:51:30 +0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,12 @@ import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Header from "./components/Header";
-import { useEffect, useState } from "react";
-import { useRef } from "react";
-
 function App() {
-  const prevScrollY = useRef(0);
-
-  const [goingUp, setGoingUp] = useState(true);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-      if (prevScrollY.current < currentScrollY && goingUp) {
-        setGoingUp(false);
-      }
-      if (prevScrollY.current > currentScrollY && !goingUp) {
-        setGoingUp(true);
-      }
-
-      prevScrollY.current = currentScrollY;
-    };
-
-    window.addEventListener("scroll", handleScroll, { passive: true });
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [goingUp]);
-
   return (
     <>
       <Router>
         <div className="App bg-primary dark:bg-dPrimary">
-          <Header goingUp={goingUp} />
+          <Header />
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
