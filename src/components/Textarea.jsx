a61e2bfa8364e6aae21d2536d2fc963fe7bbc58f
Nidal Bayan <bayan_nidal@yahoo.com>
2022-04-29 15:51:15 +0300

    build

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -20,7 +20,7 @@ export const Textarea = ({
       onChange={onChange}
       required={required}
       step={step}
-      className={`${customClass} w-full min-h-[5rem] font-medium rounded-lg text-[0.5rem] placeholder:text-[0.5rem] sm:text-base md:text-base sm:placeholder:text-[0.8rem] placeholder:md:text-base p-3 mb-2 bg-secondary dark:bg-dSecondary text-text dark:text-white caret-third placeholder:font-medium`}
+      className={`${customClass} w-full min-h-[7rem] font-medium rounded-lg text-[0.5rem] placeholder:text-[0.5rem] sm:text-base md:text-base sm:placeholder:text-[0.8rem] placeholder:md:text-base p-3 mb-2 bg-secondary dark:bg-dSecondary text-text dark:text-white caret-third placeholder:font-medium`}
     />
   );
 };
