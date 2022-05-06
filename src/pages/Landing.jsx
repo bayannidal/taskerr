7441fb66e54d373d9e1459aa33ac6f309a27a880
Nidal Bayan <bayan_nidal@yahoo.com>
2022-05-06 11:00:49 +0300

    nothing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Landing = () => {
-  return <div className="pt-24"></div>;
+  return (
+    <div className="layout min-w-full flex flex-col max-w-full min-h-full"></div>
+  );
 };
 
 export default Landing;
